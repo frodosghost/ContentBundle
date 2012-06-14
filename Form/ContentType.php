f76b9d1045e152ca0aa3085887adbe3c08095a53
James Rickard <james@frodosghost.com>
2012-06-14 21:33:01 +1000

    Updated to work with the new 2.1 build form.

--- Form/ContentType.php
+++ Form/ContentType.php
@@ -3,11 +3,11 @@
 namespace Manhattan\Bundle\ContentBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class ContentType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
@@ -24,4 +24,5 @@ class ContentType extends AbstractType
     {
         return 'content';
     }
+
 }
